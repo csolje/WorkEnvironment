ad3765a39ff8f4d2c481d5b5e007941d96ec873f
Christian Solje <christian.solje@aller.com>
2020-06-11 09:48:52 +0200

    Added Switch-AZSubscription.ps1
    Switching between subscriptions Dev,Test and Prod

---
+++ components/ps-profile/src/functions/Switch-AZSubscription.ps1
@@ -0,0 +1,17 @@
+function Switch-Subscription {
+    param (
+        # Parameter help description
+        [Parameter(Mandatory)]
+        [ValidateSet('Prod', 'Test','Dev')]
+        [string]$Subscription
+    )
+    if ($Subscription -eq 'Prod'){
+        Select-AzSubscription -SubscriptionId 'ff0ee4f9-bf7b-44c0-8bd3-eeb16c1eebea'
+    }
+    if ($Subscription -eq 'Test') {
+        Select-AzSubscription -SubscriptionId '7336106d-3254-498c-8fc6-0da086acc9d9'
+    }
+    if ($Subscription -eq 'Dev') {
+        Select-AzSubscription -SubscriptionId '9fefd134-d3c0-4745-909a-128a6ad3f5ba'
+    }
+}
